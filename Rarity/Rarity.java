3d9a16bd0ab574c96ecee949ea6e6aa341053393
Lydia Farag <77176132+lydiafarag@users.noreply.github.com>
2022-08-02 21:29:26 -0400

    Create Rarity.java

---
+++ Rarity/Rarity.java
@@ -0,0 +1,9 @@
+package Rarity;
+
+public abstract class Rarity {
+    protected String rarity;
+    protected int NumOfRarities;
+
+    public abstract String getRarity();
+    public abstract int getNumOfRarities();
+}
